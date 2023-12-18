f2800ee946eb41eb27de7f9c47b79c7707bccb09
DevBasem <engr.basemahmed@gmail.com>
2023-12-17 21:35:54 -0800

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,4 @@
 #include "sort.h"
-void swap(int *a, int *b);
-/**
- * bubble_sort - sort an array of integer in ascending order
- * @array: the array to order and print
- * @size: size of array to order
- */
-void bubble_sort(int *array, size_t size)
-{
-	unsigned int i = 0, j = 0;
-
-	if (size <= 1)
-		return;
-
-	for (i = 0; i < (size - 1); i++)
-		for (j = 1; j < size; j++)
-			if (array[j - 1] > array[j])
-			{
-				swap(&array[j - 1], &array[j]);
-				print_array(array, size);
-			}
-}
 
 /**
  * swap - swap two pointers to integer
@@ -34,4 +13,29 @@ void swap(int *a, int *b)
 	swap = *a;
 	*a = *b;
 	*b = swap;
+}
+
+/**
+ * bubble_sort - sort an array of integers in ascending order
+ * @array: the array to order and print
+ * @size: size of array to order
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+
+	if (size <= 1)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 1; j < size; j++)
+		{
+			if (array[j - 1] > array[j])
+			{
+				swap(&array[j - 1], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
 }
